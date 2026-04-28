d02053a54cb19208cc25f03bc4be8303bb1661f2
Jon Lehman <jonlehman@users.noreply.github.com>
2026-04-28 15:00:36 -0700

    Add Google Ads tag to app shell

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import "./globals.css";
+import Script from "next/script";
+
+const GOOGLE_ADS_ID = "AW-18124035745";
 
 export const metadata = {
   title: "EveryCall",
@@ -20,7 +23,22 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('set', 'linker', {'domains': ['everycall.io', 'app.everycall.io']});
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ADS_ID}');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
